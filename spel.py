de9fb4c4d59df03d3d12085958ee370cd11fa84f
hugo <hugo.lauri@skola.taby.se>
2023-11-13 09:31:42 +0100

    yo

--- spel.py
+++ spel.py
@@ -1,2 +1,14 @@
-print("hej :)")
-print("asdfakjsdfha")
+import random as rand
+
+
+def main():
+
+    print("Hello traveler, you have been captured. To escape this maze you will have to choose the right door.")
+    playername = input("What is your name?")
+    print(f"Hello {playername}")
+
+    playerhp = 100
+    playerstrength = 10
+
+
+main()
